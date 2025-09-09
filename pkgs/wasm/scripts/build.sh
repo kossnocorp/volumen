db7a7c62c7f50229403cfe5af9bd8803f0f15869
Sasha Koss <koss@nocorp.me>
2025-09-09 00:19:19 +0000

    Change publish script to ship

--- pkgs/wasm/scripts/build.sh
+++ pkgs/wasm/scripts/build.sh
@@ -16,8 +16,8 @@ echo -e "$(cat "$dir/package.json" | jaq '
   .dependencies //= {} |
   .dependencies["volumen-types"] = "workspace:^" |
   .scripts //= {} |
-  .scripts["publish"] = "pnpm publish" |
-  .scripts["publish:next"] = "pnpm publish --tag next" |
+  .scripts["ship"] = "pnpm publish" |
+  .scripts["ship:next"] = "pnpm publish --tag next" |
   .name = "volumen"
 ')" > "$dir/package.json"
 
