683ca4ea58f53cb2ccf7974a1629477f384d2bba
Sasha Koss <koss@nocorp.me>
2025-09-10 02:56:32 +0000

    Fix Wasm build script

--- pkgs/wasm/scripts/build.sh
+++ pkgs/wasm/scripts/build.sh
@@ -7,19 +7,21 @@ echo -e "⚡️ Building Wasm package\n"
 dir="../npm"
 
 find $dir -type f \
-	! -name 'package.json' \
+  ! -name 'package.json' \
 	! -name 'turbo.json' \
+	! -name 'README.md' \
 	-delete
 
 wasm-pack build --target nodejs --out-name volumen --out-dir $dir
 
-version=$(jaq -r ".version" "$package_json")
+version=$(jaq -r ".version" "./package.json")
 
-echo -e "$(cat "$dir/package.json" | jaq '
+
+echo -e "$(cat "$dir/package.json" | VERSION="$version" jaq '
   .dependencies //= {} |
   .dependencies["@volumen/types"] = "workspace:^" |
   .name = "volumen" |
-  .version = "'"$version"'" |
+  .version = env.VERSION
 ')" >"$dir/package.json"
 
 cat >"$dir/.gitignore" <<'EOF'
